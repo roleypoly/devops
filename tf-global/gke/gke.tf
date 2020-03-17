03ab17c8be4b91008d9cdd19cb4233dd85e6b796
Katalina <git@kat.cafe>
2020-03-16 21:57:03 -0400

    tf init

---
+++ tf-global/gke/gke.tf
@@ -0,0 +1,69 @@
+resource "google_container_cluster" "primary" {
+  location = "us-east1-a"
+
+  # We can't create a cluster with no node pool defined, but we want to only use
+  # separately managed node pools. So we create the smallest possible default
+  # node pool and immediately delete it.
+  remove_default_node_pool = true
+  initial_node_count       = 1
+
+  addons_config {
+    istio_config {
+      disabled = false
+    }
+  }
+
+  master_auth {
+    username = ""
+    password = ""
+
+    client_certificate_config {
+      issue_client_certificate = false
+    }
+  }
+}
+
+resource "google_container_node_pool" "primary_static_nodes" {
+  location   = "us-east1-a"
+  cluster    = google_container_cluster.primary.name
+  node_count = 3
+
+  node_config {
+    preemptible  = false
+    machine_type = "n1-standard-1"
+
+    metadata = {
+      disable-legacy-endpoints = "true"
+    }
+
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+    ]
+  }
+}
+
+resource "google_container_node_pool" "primary_preemptible_nodes" {
+  location   = "us-east1-a"
+  cluster    = google_container_cluster.primary.name
+  node_count = 0
+
+  autoscaling {
+    max_node_count = 5
+    min_node_count = 0
+  }
+
+  node_config {
+    preemptible  = true
+    machine_type = "n1-standard-1"
+
+    metadata = {
+      disable-legacy-endpoints = "true"
+    }
+
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+    ]
+  }
+}
