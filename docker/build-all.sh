25ed34e2252a1ae09ddb6d21656bac84db6943a2
Kata <git@kat.cafe>
2020-05-07 15:49:13 -0400

    fix(docker): adjust retagging

--- docker/build-all.sh
+++ docker/build-all.sh
@@ -22,23 +22,26 @@ retag() {
     tag=$1
     image=$2
     suffix=$3
-    current_branch=$(git rev-parse --abbrev-ref HEAD)
+    current_branch=$(echo ${GITHUB_REF:-$(git rev-parse --abbrev-ref HEAD)} | sed 's#refs/heads/##')
+    echo "current_branch=$current_branch"
 
-    case current_branch in
+    case $current_branch in
         master) 
-            docker tag $tag "$tag:latest$suffix" 
-            to_push="$tag:latest$suffix $to_push"
+            echo "tagged master => latest"
+            docker tag $tag "$image:latest$suffix" 
+            to_push+=" $image:latest$suffix"
             ;;
         develop) 
-            docker tag $tag "$tag:next$suffix" 
-            to_push="$tag:next$suffix $to_push"
+            echo "tagged develop => next"
+            docker tag $tag "$image:next$suffix" 
+            to_push+=" $image:next$suffix"
             ;;
     esac
 }
 
 push() {
     for image in $1; do
-        docker push $image
+        cmd docker push $image
     done
 }
 
@@ -57,7 +60,7 @@ build() {
     to_push=$full_tag
     retag $full_tag $imagename $suffix
 
-    push $to_push
+    push "$to_push"
 
     test -f "${1}_test.yml" && container_test $full_tag $context $(basename "${1}_test.yml")
 }
