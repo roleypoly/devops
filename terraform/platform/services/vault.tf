20f041bae902476558531a75a242363f0bd9baa4
Katalina <git@kat.cafe>
2020-06-18 01:30:49 -0400

    i promise i have a clusterology ph.d.

--- terraform/platform/services/vault.tf
+++ terraform/platform/services/vault.tf
@@ -81,48 +81,53 @@ resource "kubernetes_deployment" "vault" {
       match_labels = local.vaultLabels
     }
 
-
     template {
-      container {
-        image = "vault:1.4.2"
-        name  = "vault"
+      metadata {
+        labels = local.vaultLabels
+      }
 
-        volume_mount {
-          mount_path = "/vault/mounted-secrets"
-          name       = "vault-secrets"
-          read_only  = true
-        }
+      spec {
+        container {
+          image = "vault:1.4.2"
+          name  = "vault"
 
-        volume_mount {
-          mount_path = "/vault/config/vault-config.json"
-          name       = "vault-config"
-          sub_path   = "vault-config.json"
-        }
+          volume_mount {
+            mount_path = "/vault/mounted-secrets"
+            name       = "vault-secrets"
+            read_only  = true
+          }
 
-        security_context {
-          capabilities {
-            add = ["IPC_LOCK"]
+          volume_mount {
+            mount_path = "/vault/config/vault-config.json"
+            name       = "vault-config"
+            sub_path   = "vault-config.json"
+          }
+
+          security_context {
+            capabilities {
+              add = ["IPC_LOCK"]
+            }
           }
         }
-      }
 
-      node_selector = {
-        node_type = "static"
-      }
-
-      restart_policy = "Always"
-
-      volume {
-        name = "vault-secrets"
-        secret {
-          secret_name = kubernetes_secret.vault-svcacct.metadata.0.name
+        node_selector = {
+          node_type = "static"
         }
-      }
 
-      volume {
-        name = "vault-config"
-        config_map {
-          name = kubernetes_config_map.vault-cm.metadata.0.name
+        restart_policy = "Always"
+
+        volume {
+          name = "vault-secrets"
+          secret {
+            secret_name = kubernetes_secret.vault-svcacct.metadata.0.name
+          }
+        }
+
+        volume {
+          name = "vault-config"
+          config_map {
+            name = kubernetes_config_map.vault-cm.metadata.0.name
+          }
         }
       }
     }
